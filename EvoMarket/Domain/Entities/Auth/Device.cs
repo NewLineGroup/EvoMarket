0b768c9d401c49528d459699adb9886c7e80638e
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-09-20 09:45:58 -0700

    Changed

--- EvoMarket/Domain/Entities/Auth/Device.cs
+++ EvoMarket/Domain/Entities/Auth/Device.cs
@@ -2,8 +2,8 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities.Auth;
-[Table("devices")]
 
+[Table("devices")]
 public class Device:ModelBase
 {
     [Required]
