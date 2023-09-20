a775ef6e5a22a64e4b8c75feb6d60d57c37abf5e
Azimjon <azimjonshodiyorov@gmail.com>
2023-09-20 20:57:22 +0500

    Paymet model added

---
+++ EvoMarket/Domain/Entities/Payment/ClientAccount.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Payment;
+
+[Table("client_account")]
+public class ClientAccount
+{
+    [Required]
+    [Column("client_id")]
+    public long ClientId { get; set; }
+    [Column("balance")]
+    public decimal Balance { get; set; }
+    [Column("is_freeze")]
+    public bool IsFreeze { get; set; }
+}
