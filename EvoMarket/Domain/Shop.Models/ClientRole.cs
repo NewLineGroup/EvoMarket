60c5387846c88361207fa2dfbe73b297beadf746
sobirxazratov <xazratovsobir3@gamil.com>
2023-09-20 05:06:18 -0700

    Created Client Models

--- EvoMarket/Domain/Shop.Models/ClientRole.cs
+++ EvoMarket/Domain/Shop.Models/ClientRole.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Domain.Shop.Models;
 
+[Table("clients_role",Schema = "shop")]
 public class ClientRole : ModelBase
 {
+    [Column("name")]
     public string Name { get; set; }
+    [Column("active")]
     public bool Active { get; set; }
 }
