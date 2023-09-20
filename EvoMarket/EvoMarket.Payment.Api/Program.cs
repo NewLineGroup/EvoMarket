ab65c406436de04e65439fdfd9bb31674222a0db
Jamolxon <jamolxon002.iz@gmail.com>
2023-09-20 22:57:51 +0500

    Enviroment Automaticly configure added

--- EvoMarket/EvoMarket.Payment.Api/Program.cs
+++ EvoMarket/EvoMarket.Payment.Api/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Host.ConfigureHostConfiguration(configurationBuilder =>
+{
+    configurationBuilder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
