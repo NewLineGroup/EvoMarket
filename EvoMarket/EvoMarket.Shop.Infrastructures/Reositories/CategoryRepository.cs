4e5840d49a90655cb5f8967c815b59bf4dca324c
sobirxazratov <xazratovsobir3@gamil.com>
2023-09-20 10:36:27 -0700

    Created ICategoryRepositories and CategoryRepository

---
+++ EvoMarket/EvoMarket.Shop.Infrastructures/Reositories/CategoryRepository.cs
@@ -0,0 +1,13 @@
+using Domain.Entities.Shops;
+using EvoMarket.WebCore.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Shop.Interfaces;
+
+namespace Shop.Reositories;
+
+public class CategoryRepository : RepositoryBase<Category> , ICategoryRepository
+{
+    public CategoryRepository(DbContext context) : base(context)
+    {
+    }
+}
