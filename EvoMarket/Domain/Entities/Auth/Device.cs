f3e3aa4fbb1a6b8019c5802d44a93efe69921f56
Fayzulloabdullayev09 <141065669+Fayzulloabdullayev09@users.noreply.github.com>
2023-09-20 09:43:59 -0700

    Rename Auth

--- EvoMarket/Domain/Entities/Auth/Device.cs
+++ EvoMarket/Domain/Entities/Auth/Device.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Domain.Entities.AuthModels;
+namespace Domain.Entities.Auth;
 [Table("devices")]
 
 public class Device:ModelBase
