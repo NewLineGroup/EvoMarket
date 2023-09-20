65e12b986dd89f67e6b1dc5404c418b80f6c80d3
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-09-20 20:54:50 +0500

    added model

---
+++ EvoMarket/Domain/Domain/Entities/ClientNotification.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities;
+
+[Table("client_notifications")]
+public class ClientNotifications : ModelBase
+{
+    [Column("client_id")]
+    public int ClientId { get; set; }
+    
+    // [NotMapped]
+    //public Client Client { get; set; }
+    
+    [Column("message_data")]
+    public DateTime MessageData { get; set; }
+
+    [Column("message")]
+    public string message { get; set; }
+
+    [Column("received")]
+    public bool Received { get; set; }
+    
+}
