3e42c2565e0560e9776cfb812e567be46c8e3dde
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-09-20 21:15:02 +0500

    created models

---
+++ EvoMarket/Domain/Entities/Shops/Category.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace Domain.Entities.Shops;
+
+[Table("categories",Schema = "shop")]
+public class Category:ModelBase
+{
+    [Column("title")]
+    public string Title { get; set; }
+    [Column("image_url")]
+    public string ImageUrl { get; set; }
+    [Column("category_filters")]
+    public CategoryFilters CategoryFilters { get; set; }
+    
+    
+}
+
