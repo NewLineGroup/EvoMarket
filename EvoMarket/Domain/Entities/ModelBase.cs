3916c89e15c538a262ea56dd2de74cf8cd281b4d
Azimjon <azimabdishukurov@gmail.com>
2023-09-21 21:14:47 +0500

    Abstract is removed

--- EvoMarket/Domain/Entities/ModelBase.cs
+++ EvoMarket/Domain/Entities/ModelBase.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities;
 
-public abstract class ModelBase
+public class ModelBase
 {
     [Column("id")] public long Id { get; set; }
 }
