c48d1d83982a294d7bfe00e64f45711a1fefae52
Norov Feruzbek <norovferuzbek3963@gmail.com>
2023-09-21 22:47:41 +0300

    Update

--- EvoMarket/EvoMarket.Infrastructure/DbContexts/ShopDataContext.cs
+++ EvoMarket/EvoMarket.Infrastructure/DbContexts/ShopDataContext.cs
@@ -8,6 +8,7 @@ public class ShopDataContext:DbContext
     public DbSet<Cart> Carts { get; set; }
     public DbSet<Client> Clients { get; set; }
     public DbSet<Category> Categories { get; set; }
+    
     public DbSet<Product> Products { get; set; }
 
     public ShopDataContext(DbContextOptions<ShopDataContext>options):base(options)
