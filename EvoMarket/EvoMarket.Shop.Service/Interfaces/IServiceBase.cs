19fb13ec21ca6c2a7eb09e0c6ec618d723355635
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-09-21 17:42:00 +0500

    created services application and service base

---
+++ EvoMarket/EvoMarket.Shop.Service/Interfaces/IServiceBase.cs
@@ -0,0 +1,12 @@
+﻿namespace EvoMarket.Shop.Service.Interfaces;
+
+public interface IServiceBase<T>
+{
+    public ValueTask<IEnumerable<T>> GetAll();
+    public ValueTask<T> GetById(long id);
+    public ValueTask<T> Create(T data);
+    public ValueTask<T> Update(T data);
+    public ValueTask<T> Delete(T data);
+    public ValueTask<T> Delete(long id);
+
+}
