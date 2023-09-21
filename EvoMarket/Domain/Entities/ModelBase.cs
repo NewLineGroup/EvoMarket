c38b0fbbb198f46854a7264772d71859ca7adb1c
Azimjon <azimabdishukurov@gmail.com>
2023-09-21 21:11:34 +0500

    Last Update

--- EvoMarket/Domain/Entities/ModelBase.cs
+++ EvoMarket/Domain/Entities/ModelBase.cs
@@ -5,6 +5,4 @@ namespace Domain.Entities;
 public abstract class ModelBase
 {
     [Column("id")] public long Id { get; set; }
-    [Column("create_date")] public DateTime CreateDate { get; set; }
-    [Column("update_date")] public DateTime UpdateDate { get; set; }
 }
