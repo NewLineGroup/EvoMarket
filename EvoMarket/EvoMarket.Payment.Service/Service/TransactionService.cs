bd5bb00288f99a220dd3f5407ff10413c5b6231d
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-09-21 19:56:19 +0500

    update TransactionService

--- EvoMarket/EvoMarket.Payment.Service/Service/TransactionService.cs
+++ EvoMarket/EvoMarket.Payment.Service/Service/TransactionService.cs
@@ -1,5 +1,6 @@
 using Domain.Entities.Payment;
 using EvoMarket.Payment.Infrastructure.Intercafes;
+using EvoMarket.Payment.Infrastructure.PaymentDataContext;
 using Microsoft.EntityFrameworkCore;
 
 namespace EvoMarket.Payment.Service.Service;
@@ -8,57 +9,77 @@ public class TransactionService
 {
     private readonly ITransactionRepository _transactionRepository;
     private readonly IClientAccountRepository _clientAccountRepository;
+    private readonly PaymentDbContext _context;
 
-    public TransactionService(ITransactionRepository transactionRepository,IClientAccountRepository clientAccountRepository)
+    public TransactionService(ITransactionRepository transactionRepository,IClientAccountRepository clientAccountRepository,PaymentDbContext context)
     {
         _transactionRepository = transactionRepository;
         _clientAccountRepository = clientAccountRepository;
+        _context = context;
     }
 
 
-    public async ValueTask<Transaction> Transaction(long clientId, decimal Money,decimal foiz)
+    public async ValueTask<Transaction> Transaction(long clientId, decimal money, decimal foiz)
     {
-        if (Money <= 0 || foiz < 0)
+        var result = new Transaction()
         {
-            throw new Exception("Money or foiz can be negative");
-            return null;
-        }
-
-        var clientAccount = await _clientAccountRepository.GetByIdAsync(clientId);
-
-        if (clientAccount is null)
+            Time = DateTime.Now
+        };
+        using (var transaction = await _context.Database.BeginTransactionAsync())
         {
-            throw new Exception("Client Account not fount");
-            return null;
-        }
-
-        decimal balance = clientAccount.Balance;
-
-        if (balance < Money * foiz)
-        {
-            return await _transactionRepository.CreatAsync(new Transaction()
+            try
             {
-                Account = clientAccount,
-                AccountId = clientAccount.Id,
-                Time = DateTime.Now,
-                Amount = Money * foiz,
-                Success = false
-            });
+
+                
+                if (money <= 0 || foiz < 0)
+                {
+                    result.Success = false;
+                    throw new Exception("Money or foiz can be negative");
+                }
+
+                var clientAccount = await _clientAccountRepository.GetByIdAsync(clientId);
+
+                if (clientAccount is null)
+                {
+                    result.Success = false;
+                    throw new Exception("Client Account not fount");
+                }
+
+                decimal balance = clientAccount.Balance;
+
+                if (balance < money * foiz)
+                {
+                    result.Success = false;
+                    result.Account = clientAccount;
+                    result.AccountId = clientAccount.Id;
+
+                    throw new Exception("not enough money");
+
+                }
+
+                clientAccount.Balance -= money * foiz;
+
+                await _clientAccountRepository.UpdateAsync(clientAccount);
+
+                result.AccountId = clientAccount.Id;
+                result.Account = clientAccount;
+                result.Amount = money * foiz;
+                result.Success = true;
+
+                await _transactionRepository.UpdateAsync(result);
+
+                await _context.Database.CommitTransactionAsync();
+
+                return result;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                await _context.Database.RollbackTransactionAsync();
+                return result;
+            }
+            
         }
-
-        clientAccount.Balance -= Money * foiz;
-
-        await _clientAccountRepository.UpdateAsync(clientAccount);
-        var transaction = await _transactionRepository.CreatAsync(new Transaction()
-        {
-            Amount = Money * foiz,
-            Account = clientAccount,
-            AccountId = clientAccount.Id,
-            Time = DateTime.Now,
-            Success = true
-        });
-
-
-        return transaction;
-    } 
+    }
 }
