234281aede476946723e2cdedfdf79d07cf0c8f9
Developer <golibjonabdurasulov47@gmail.com>
2023-09-21 05:01:50 -0700

    Changed

---
+++ EvoMarket/EvoMarket.Auth.Infrastructure/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities.Auth;
+using EvoMarket.WebCore.Interfaces;
+
+namespace EvoMarket.Auth.Infrastructure.Interfaces;
+
+public interface IUserRepository:IRepositoryBase<User>
+{
+    
+}
--- EvoMarket/EvoMarket.Auth.Infrastructure/Repositories/UserRepository.cs
+++ EvoMarket/EvoMarket.Auth.Infrastructure/Repositories/UserRepository.cs
@@ -1,10 +1,11 @@
 using Domain.Entities.Auth;
+using EvoMarket.Auth.Infrastructure.Interfaces;
 using EvoMarket.WebCore.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace EvoMarket.Auth.Infrastructure.Repositories;
 
-public class UserRepository:RepositoryBase<User>
+public class UserRepository:RepositoryBase<User>,IUserRepository
 {
     public UserRepository(DbContext context) : base(context)
     {
