d4ec736704aeeea8bab6f4bcfffb6de1ad700220
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-09-21 21:49:13 +0500

    update cart and cart item

---
+++ EvoMarket/Domain/Entities/Shops/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities.Shops;
+
+[Table("cart_items", Schema = "shop")]
+public class CartItem : ModelBase
+{
+    [Column("product_id")] public long ProductId { get; set; }
+    public Product Product { get; set; }
+    [Column("cart_id")] public long CartId { get; set; }
+    public Cart Cart { get; set; }
+    [Column("quantity")] public int Quantity { get; set; }
+}
