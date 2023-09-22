09258a1af1fe682555a1b7c69080e17c9c0b5a1e
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-09-22 21:51:18 +0500

    __

--- EvoMarket/EvoMarket.Payment.Api/Controllers/TransactionController.cs
+++ EvoMarket/EvoMarket.Payment.Api/Controllers/TransactionController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Domain.Dto.Payment.TransactionDto;
 using Domain.Entities.Payment;
--- EvoMarket/EvoMarket.Payment.Service/Repositories/TransactionRepository.cs
+++ EvoMarket/EvoMarket.Payment.Service/Repositories/TransactionRepository.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Domain.Entities.Payment;
 using EvoMarket.Infrastructure.DbContexts;
 using EvoMarket.Payment.Infrastructure.Intercafes;
