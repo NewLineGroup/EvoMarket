691e132115767080d354bd661311c8aac684cd9e
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-09-23 22:16:43 +0500

    add production json

--- EvoMarket/EvoMarket.Shop.Api/Program.cs
+++ EvoMarket/EvoMarket.Shop.Api/Program.cs
@@ -2,6 +2,8 @@ using EvoMarket.Shop.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls(builder.Configuration.GetSection("Urls:WebHost").Get<string>());    
+    
 // Add services to the container.
 
 builder.Services.ConfigureDbContexts(builder.Configuration);
@@ -12,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
